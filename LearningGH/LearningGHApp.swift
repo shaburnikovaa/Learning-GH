83f30dfc430b568bc10a289c90aecb0594af3e65
Anastasia Shaburnikova <shaburnikovaa@gmail.com>
2023-01-26 12:40:56 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Taras Savytskyi on 2023-01-26.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
